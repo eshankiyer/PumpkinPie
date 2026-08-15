82f4d4365f6e75657ccb05473a2bd858af473dc7
Eshan Iyer <79593316+eshankiyer@users.noreply.github.com>
2026-08-14 20:13:35 -0400

    fix(ai): match guardian targets

--- crates/pumpkin/src/entity/mob/guardian.rs
+++ crates/pumpkin/src/entity/mob/guardian.rs
@@ -25,6 +25,7 @@ impl GuardianEntity {
             let mob_arc: Arc<dyn Mob> = mob_arc.clone();
             Arc::downgrade(&mob_arc)
         };
+        let target_weak = mob_weak.clone();
 
         {
             let mut goal_selector = mob_arc.mob_entity.goals_selector.lock().unwrap();
@@ -48,19 +49,35 @@ impl GuardianEntity {
             let mut target_selector = mob_arc.mob_entity.target_selector.lock().unwrap();
             target_selector.add_goal(
                 1,
-                ActiveTargetGoal::with_default(&mob_arc.mob_entity, &EntityType::PLAYER, true),
-            );
-            target_selector.add_goal(
-                2,
-                ActiveTargetGoal::with_default(&mob_arc.mob_entity, &EntityType::SQUID, true),
-            );
-            target_selector.add_goal(
-                2,
-                ActiveTargetGoal::with_default(&mob_arc.mob_entity, &EntityType::GLOW_SQUID, true),
-            );
-            target_selector.add_goal(
-                3,
-                ActiveTargetGoal::with_default(&mob_arc.mob_entity, &EntityType::AXOLOTL, true),
+                Box::new(ActiveTargetGoal::new_types(
+                    &mob_arc.mob_entity,
+                    &[
+                        &EntityType::PLAYER,
+                        &EntityType::SQUID,
+                        &EntityType::GLOW_SQUID,
+                        &EntityType::AXOLOTL,
+                    ],
+                    10,
+                    true,
+                    false,
+                    Some(
+                        move |target: Arc<crate::entity::living::LivingEntity>,
+                              _world: Arc<crate::world::World>| {
+                            let target_weak = target_weak.clone();
+                            async move {
+                                let Some(guardian) = target_weak.upgrade() else {
+                                    return false;
+                                };
+                                guardian
+                                    .get_entity()
+                                    .pos
+                                    .load()
+                                    .squared_distance_to_vec(&target.entity.pos.load())
+                                    > 9.0
+                            }
+                        },
+                    ),
+                )),
             );
         };
 
